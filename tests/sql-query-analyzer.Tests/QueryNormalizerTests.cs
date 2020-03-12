f3820e593464be48520d1db05ebf33793e43e070
Vladyslav Zaiets <rutova2@gmail.com>
2020-03-12 07:38:25 +0200

    reshuffle QureyNormalizerTests a bit

---
+++ tests/sql-query-analyzer.Tests/QueryNormalizerTests.cs
@@ -0,0 +1,70 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using FluentAssertions;
+using SqlQueryAnalyzer.Utilities;
+using Xunit;
+
+namespace SqlQueryAnalyzer.Tests;
+
+public class QueryNormalizerTests
+{
+    private readonly QueryNormalizer _normalizer = new();
+
+    [Fact]
+    public void Normalize_WhitespaceOnlyInput_ReturnsInputUnchanged()
+    {
+        // Arrange
+        var input = "   ";
+
+        // Act
+        var result = _normalizer.Normalize(input);
+
+        // Assert
+        result.Should().Be(input);
+    }
+
+    [Fact]
+    public void Normalize_LowercaseSqlKeywords_ConvertsKeywordsToUppercase()
+    {
+        // Arrange
+        var query = "select id from users where id = 1";
+
+        // Act
+        var result = _normalizer.Normalize(query);
+
+        // Assert
+        result.Should().Contain("SELECT")
+            .And.Contain("FROM")
+            .And.Contain("WHERE");
+    }
+
+    [Fact]
+    public void Normalize_StringLiteralInQuery_LiteralCaseIsPreserved()
+    {
+        // Arrange - 'active user' must survive keyword uppercasing untouched
+        var query = "select name from users where status = 'active user'";
+
+        // Act
+        var result = _normalizer.Normalize(query);
+
+        // Assert
+        result.Should().Contain("'active user'");
+        result.Should().Contain("SELECT").And.Contain("FROM");
+    }
+
+    [Fact]
+    public void ExtractTableNames_QueryWithFromAndJoin_ReturnsBothTableNames()
+    {
+        // Arrange
+        var query = "SELECT u.id FROM users u JOIN orders o ON u.id = o.user_id";
+
+        // Act
+        var tables = _normalizer.ExtractTableNames(query);
+
+        // Assert
+        tables.Should().Contain("users").And.Contain("orders");
+    }
+}
