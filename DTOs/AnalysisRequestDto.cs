34de41c530ff02399a6220709499f6986aec73b4
Vladyslav Zaiets <rutova2@gmail.com>
2020-05-06 16:33:21 +0300

    refactor: cleanup

---
+++ DTOs/AnalysisRequestDto.cs
@@ -0,0 +1,131 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Collections.Generic;
+
+namespace SqlQueryAnalyzer.DTOs;
+
+/// <summary>
+/// Request DTO for analyzing a query
+/// </summary>
+public class AnalysisRequestDto
+{
+    public string QueryText { get; set; } = string.Empty;
+    public string? ApplicationName { get; set; }
+    public string? ProcedureName { get; set; }
+    public string? ModuleName { get; set; }
+    public bool IncludeIndexSuggestions { get; set; } = true;
+    public bool AnalyzeFragmentation { get; set; } = true;
+    public bool AnalyzePlan { get; set; } = false;
+    public string? ExecutionPlanXml { get; set; }
+}
+
+/// <summary>
+/// Response DTO for analysis results
+/// </summary>
+public class AnalysisResponseDto
+{
+    public string QueryId { get; set; } = string.Empty;
+    public double PerformanceScore { get; set; }
+    public string ComplexityLevel { get; set; } = string.Empty;
+    public int IssueCount { get; set; }
+    public int CriticalIssueCount { get; set; }
+    public List<PerformanceIssueDto> Issues { get; set; } = [];
+    public List<IndexSuggestionDto> IndexSuggestions { get; set; } = [];
+    public string Summary { get; set; } = string.Empty;
+    public long AnalysisTimeMs { get; set; }
+}
+
+/// <summary>
+/// DTO for performance issue
+/// </summary>
+public class PerformanceIssueDto
+{
+    public string IssueType { get; set; } = string.Empty;
+    public string Severity { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public double EstimatedImpact { get; set; }
+    public string RecommendedFix { get; set; } = string.Empty;
+    public int Priority { get; set; }
+}
+
+/// <summary>
+/// DTO for index suggestion
+/// </summary>
+public class IndexSuggestionDto
+{
+    public string TableName { get; set; } = string.Empty;
+    public string IndexName { get; set; } = string.Empty;
+    public List<string> Columns { get; set; } = [];
+    public List<string> IncludeColumns { get; set; } = [];
+    public double EstimatedGain { get; set; }
+    public string CreateScript { get; set; } = string.Empty;
+    public int AffectedQueries { get; set; }
+}
+
+/// <summary>
+/// DTO for batch analysis request
+/// </summary>
+public class BatchAnalysisRequestDto
+{
+    public List<string> Queries { get; set; } = [];
+    public string? ApplicationName { get; set; }
+    public bool AnalyzePatterns { get; set; } = true;
+    public int TimeoutSeconds { get; set; } = 300;
+}
+
+/// <summary>
+/// DTO for batch analysis response
+/// </summary>
+public class BatchAnalysisResponseDto
+{
+    public int TotalQueries { get; set; }
+    public int SuccessfulAnalyses { get; set; }
+    public int FailedAnalyses { get; set; }
+    public List<AnalysisResponseDto> Results { get; set; } = [];
+    public List<string> NPlusOnePatterns { get; set; } = [];
+    public double AverageScore { get; set; }
+    public long TotalAnalysisTimeMs { get; set; }
+}
+
+/// <summary>
+/// DTO for index analysis request
+/// </summary>
+public class IndexAnalysisRequestDto
+{
+    public string TableName { get; set; } = string.Empty;
+    public bool IncludeFragmentation { get; set; } = true;
+    public bool IncludeUnused { get; set; } = true;
+    public bool GenerateScripts { get; set; } = true;
+}
+
+/// <summary>
+/// DTO for index analysis response
+/// </summary>
+public class IndexAnalysisResponseDto
+{
+    public string TableName { get; set; } = string.Empty;
+    public int TotalIndexes { get; set; }
+    public int UnusedCount { get; set; }
+    public int FragmentedCount { get; set; }
+    public List<IndexDetailDto> Indexes { get; set; } = [];
+    public List<IndexSuggestionDto> Suggestions { get; set; } = [];
+    public List<string> MaintenanceScripts { get; set; } = [];
+}
+
+/// <summary>
+/// DTO for index details
+/// </summary>
+public class IndexDetailDto
+{
+    public string IndexName { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public List<string> Columns { get; set; } = [];
+    public long SizeKB { get; set; }
+    public double FragmentationPercent { get; set; }
+    public long TotalUsageCount { get; set; }
+    public string HealthStatus { get; set; } = string.Empty;
+    public string RiskLevel { get; set; } = string.Empty;
+}
---
+++ Diagnostics/AnalyzerHealthCheck.cs
@@ -0,0 +1,324 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using Microsoft.Extensions.Logging;
+using SqlQueryAnalyzer.Caching;
+using SqlQueryAnalyzer.Middleware;
+using SqlQueryAnalyzer.Utilities;
+
+namespace SqlQueryAnalyzer.Diagnostics;
+
+/// <summary>
+/// Health check system for the SQL query analyzer.
+/// Monitors component status, resource usage, and system health.
+/// Provides diagnostics and self-healing capabilities.
+/// </summary>
+public class AnalyzerHealthCheck
+{
+    private readonly ILogger<AnalyzerHealthCheck> _logger;
+    private readonly QueryAnalysisCache _cache;
+    private readonly RateLimitingMiddleware _rateLimiter;
+    private readonly PerformanceMetricCollector _metrics;
+    private readonly DatabaseConnectionValidator _connectionValidator;
+
+    public AnalyzerHealthCheck(
+        ILogger<AnalyzerHealthCheck> logger,
+        QueryAnalysisCache cache,
+        RateLimitingMiddleware rateLimiter,
+        PerformanceMetricCollector metrics,
+        DatabaseConnectionValidator connectionValidator)
+    {
+        _logger = logger;
+        _cache = cache;
+        _rateLimiter = rateLimiter;
+        _metrics = metrics;
+        _connectionValidator = connectionValidator;
+    }
+
+    /// <summary>
+    /// Performs comprehensive health check of all components.
+    /// </summary>
+    public async Task<HealthCheckResult> CheckHealthAsync()
+    {
+        var result = new HealthCheckResult();
+        result.CheckTime = DateTime.UtcNow;
+
+        try
+        {
+            // Check cache health
+            result.CacheHealth = CheckCacheHealth();
+
+            // Check rate limiter
+            result.RateLimiterHealth = CheckRateLimiterHealth();
+
+            // Check metrics collection
+            result.MetricsHealth = CheckMetricsHealth();
+
+            // Check database connectivity
+            result.DatabaseHealth = await CheckDatabaseHealthAsync();
+
+            // Determine overall health
+            result.Status = DetermineOverallHealth(result);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error performing health check");
+            result.Status = HealthStatus.Error;
+            result.Errors.Add($"Health check error: {ex.Message}");
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Checks cache component health.
+    /// </summary>
+    private ComponentHealth CheckCacheHealth()
+    {
+        var health = new ComponentHealth { Component = "Cache" };
+
+        try
+        {
+            var stats = _cache.GetStatistics();
+
+            // Cache is healthy if hit rate is reasonable or cache is mostly empty
+            if (stats.HitRate >= 30 || stats.TotalEntries == 0)
+            {
+                health.Status = HealthStatus.Healthy;
+                health.Message = $"Cache operational ({stats.HitRate:F1}% hit rate)";
+            }
+            else
+            {
+                health.Status = HealthStatus.Degraded;
+                health.Message = $"Low cache hit rate ({stats.HitRate:F1}%)";
+            }
+
+            // Warn if cache is near capacity
+            if (stats.TotalEntries > stats.MaxEntries * 0.9)
+            {
+                health.Status = HealthStatus.Degraded;
+                health.Message = "Cache near capacity";
+            }
+        }
+        catch (Exception ex)
+        {
+            health.Status = HealthStatus.Error;
+            health.Message = $"Cache check failed: {ex.Message}";
+        }
+
+        return health;
+    }
+
+    /// <summary>
+    /// Checks rate limiter health.
+    /// </summary>
+    private ComponentHealth CheckRateLimiterHealth()
+    {
+        var health = new ComponentHealth { Component = "RateLimiter" };
+
+        try
+        {
+            var load = _rateLimiter.GetSystemLoad();
+
+            if (load < 70)
+            {
+                health.Status = HealthStatus.Healthy;
+                health.Message = $"Rate limiter operational (load: {load:F1}%)";
+            }
+            else if (load < 90)
+            {
+                health.Status = HealthStatus.Degraded;
+                health.Message = $"High load ({load:F1}% of capacity)";
+            }
+            else
+            {
+                health.Status = HealthStatus.Unhealthy;
+                health.Message = $"Critical load ({load:F1}% of capacity)";
+            }
+        }
+        catch (Exception ex)
+        {
+            health.Status = HealthStatus.Error;
+            health.Message = $"Rate limiter check failed: {ex.Message}";
+        }
+
+        return health;
+    }
+
+    /// <summary>
+    /// Checks metrics collection health.
+    /// </summary>
+    private ComponentHealth CheckMetricsHealth()
+    {
+        var health = new ComponentHealth { Component = "Metrics" };
+
+        try
+        {
+            var report = _metrics.GetReport();
+
+            if (report.SuccessRate >= 95)
+            {
+                health.Status = HealthStatus.Healthy;
+                health.Message = $"Metrics healthy (success rate: {report.SuccessRate:F1}%)";
+            }
+            else
+            {
+                health.Status = HealthStatus.Degraded;
+                health.Message = $"Lower success rate ({report.SuccessRate:F1}%)";
+            }
+        }
+        catch (Exception ex)
+        {
+            health.Status = HealthStatus.Error;
+            health.Message = $"Metrics check failed: {ex.Message}";
+        }
+
+        return health;
+    }
+
+    /// <summary>
+    /// Checks database connectivity.
+    /// </summary>
+    private async Task<ComponentHealth> CheckDatabaseHealthAsync()
+    {
+        var health = new ComponentHealth { Component = "Database" };
+
+        try
+        {
+            // Validate connection without actual connection for speed
+            var validation = await _connectionValidator.ValidateAsync(
+                "test", // Would use actual connection string in production
+                testConnection: false);
+
+            if (validation.IsValid)
+            {
+                health.Status = HealthStatus.Healthy;
+                health.Message = "Database connection valid";
+            }
+            else
+            {
+                health.Status = HealthStatus.Unhealthy;
+                health.Message = $"Database validation failed: {string.Join(", ", validation.Errors)}";
+            }
+        }
+        catch (Exception ex)
+        {
+            health.Status = HealthStatus.Error;
+            health.Message = $"Database check error: {ex.Message}";
+        }
+
+        return health;
+    }
+
+    /// <summary>
+    /// Determines overall health status based on component statuses.
+    /// </summary>
+    private HealthStatus DetermineOverallHealth(HealthCheckResult result)
+    {
+        var components = new[] { result.CacheHealth, result.RateLimiterHealth, result.MetricsHealth, result.DatabaseHealth };
+
+        // If any critical component is unhealthy, overall is unhealthy
+        if (components.Any(c => c.Status == HealthStatus.Unhealthy || c.Status == HealthStatus.Error))
+        {
+            return HealthStatus.Unhealthy;
+        }
+
+        // If any component is degraded, overall is degraded
+        if (components.Any(c => c.Status == HealthStatus.Degraded))
+        {
+            return HealthStatus.Degraded;
+        }
+
+        return HealthStatus.Healthy;
+    }
+
+    /// <summary>
+    /// Attempts to self-heal degraded components.
+    /// </summary>
+    public async Task<SelfHealResult> AttemptSelfHealAsync(HealthCheckResult healthCheck)
+    {
+        var result = new SelfHealResult();
+
+        try
+        {
+            // Clear cache if health is degraded
+            if (healthCheck.CacheHealth.Status == HealthStatus.Degraded)
+            {
+                _cache.RemoveExpiredEntries();
+                result.ActionsPerformed.Add("Cleared expired cache entries");
+            }
+
+            // Reset metrics if needed
+            if (healthCheck.MetricsHealth.Status == HealthStatus.Degraded)
+            {
+                _metrics.Reset();
+                result.ActionsPerformed.Add("Reset performance metrics");
+            }
+
+            result.Success = true;
+            _logger.LogInformation($"Self-heal performed {result.ActionsPerformed.Count} actions");
+        }
+        catch (Exception ex)
+        {
+            result.Success = false;
+            result.Error = ex.Message;
+            _logger.LogError(ex, "Self-heal failed");
+        }
+
+        return result;
+    }
+}
+
+/// <summary>
+/// Health check result containing status of all components.
+/// </summary>
+public class HealthCheckResult
+{
+    public DateTime CheckTime { get; set; }
+    public HealthStatus Status { get; set; }
+    public ComponentHealth CacheHealth { get; set; } = new();
+    public ComponentHealth RateLimiterHealth { get; set; } = new();
+    public ComponentHealth MetricsHealth { get; set; } = new();
+    public ComponentHealth DatabaseHealth { get; set; } = new();
+    public List<string> Errors { get; set; } = new();
+
+    public override string ToString() =>
+        $"Health Status: {Status}\n" +
+        $"  Cache: {CacheHealth.Status}\n" +
+        $"  RateLimiter: {RateLimiterHealth.Status}\n" +
+        $"  Metrics: {MetricsHealth.Status}\n" +
+        $"  Database: {DatabaseHealth.Status}";
+}
+
+/// <summary>
+/// Health status of a single component.
+/// </summary>
+public class ComponentHealth
+{
+    public string Component { get; set; } = string.Empty;
+    public HealthStatus Status { get; set; }
+    public string Message { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// Result of self-healing attempt.
+/// </summary>
+public class SelfHealResult
+{
+    public bool Success { get; set; }
+    public List<string> ActionsPerformed { get; set; } = new();
+    public string? Error { get; set; }
+}
+
+/// <summary>
+/// System health status enum.
+/// </summary>
+public enum HealthStatus
+{
+    Healthy,
+    Degraded,
+    Unhealthy,
+    Error
+}
