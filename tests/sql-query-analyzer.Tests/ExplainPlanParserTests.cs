1f14c8e297adfbf738c9fe1087ae6462bf605758
Vladyslav Zaiets <rutova2@gmail.com>
2020-06-04 07:12:57 +0300

    tighten guard clauses in ExplainPlanParserTests

--- tests/sql-query-analyzer.Tests/ExplainPlanParserTests.cs
+++ tests/sql-query-analyzer.Tests/ExplainPlanParserTests.cs
@@ -15,12 +15,31 @@ using Xunit;
 
 namespace SqlQueryAnalyzer.Tests;
 
+/// <summary>
+/// Unit tests for the <see cref="ExplainPlanParserService"/> class.
+/// Tests parsing of PostgreSQL EXPLAIN plan JSON output and validation of parsed results.
+/// </summary>
 public class ExplainPlanParserTests
 {
+    /// <summary>
+    /// Mock service for testing <see cref="IQueryPlanAnalyzerService"/> dependencies.
+    /// </summary>
     private readonly Mock<IQueryPlanAnalyzerService> _mockPlanAnalyzerService;
+
+    /// <summary>
+    /// Mock logger for testing <see cref="ILogger"/> dependencies.
+    /// </summary>
     private readonly Mock<ILogger<ExplainPlanParserService>> _mockLogger;
+
+    /// <summary>
+    /// System under test - the <see cref="ExplainPlanParserService"/> instance being tested.
+    /// </summary>
     private readonly ExplainPlanParserService _sut;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ExplainPlanParserTests"/> class.
+    /// Sets up mock dependencies for testing the <see cref="ExplainPlanParserService"/> class.
+    /// </summary>
     public ExplainPlanParserTests()
     {
         _mockPlanAnalyzerService = new Mock<IQueryPlanAnalyzerService>();
@@ -28,6 +47,11 @@ public class ExplainPlanParserTests
         _sut = new ExplainPlanParserService(_mockPlanAnalyzerService.Object, _mockLogger.Object);
     }
 
+    /// <summary>
+    /// Tests parsing of PostgreSQL EXPLAIN plan in PostgreSQL 17 format.
+    /// Validates that the parser correctly extracts plan metrics from a standard PostgreSQL 17 JSON plan format.
+    /// </summary>
+    /// <returns>An awaitable task containing the parsed <see cref="QueryPlan"/> object.</returns>
     [Fact]
     public async Task ParsePostgreSqlPlan_Postgres17Format_ReturnsCorrectPlan()
     {
@@ -70,6 +94,11 @@ public class ExplainPlanParserTests
         result.TotalElapsedTime.TotalMilliseconds.Should().Be(0.300); // Execution Time
     }
 
+    /// <summary>
+    /// Tests parsing of an empty PostgreSQL EXPLAIN plan JSON array.
+    /// Validates that the parser returns a default plan with zero values when given empty input.
+    /// </summary>
+    /// <returns>An awaitable task containing the parsed <see cref="QueryPlan"/> object with default values.</returns>
     [Fact]
     public async Task ParsePostgreSqlPlan_EmptyJson_ReturnsDefaultPlan()
     {
@@ -89,6 +118,11 @@ public class ExplainPlanParserTests
         result.TotalElapsedTime.Should().Be(TimeSpan.Zero);
     }
 
+    /// <summary>
+    /// Tests parsing of invalid PostgreSQL EXPLAIN plan JSON.
+    /// Validates that the parser logs an error and returns a default plan when given malformed JSON input.
+    /// </summary>
+    /// <returns>An awaitable task containing the parsed <see cref="QueryPlan"/> object with default values.</returns>
     [Fact]
     public async Task ParsePostgreSqlPlan_InvalidJson_LogsErrorAndReturnsDefaultPlan()
     {
