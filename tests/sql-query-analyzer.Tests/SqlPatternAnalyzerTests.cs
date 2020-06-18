23a752ce049ff04753fefb8de86436896728c7a1
Vladyslav Zaiets <rutova2@gmail.com>
2020-06-18 20:50:34 +0300

    tighten guard clauses in SqlPatternAnalyzerTests

--- tests/sql-query-analyzer.Tests/SqlPatternAnalyzerTests.cs
+++ tests/sql-query-analyzer.Tests/SqlPatternAnalyzerTests.cs
@@ -10,8 +10,14 @@ using Xunit;
 
 namespace SqlQueryAnalyzer.Tests;
 
+/// <summary>
+/// Provides unit tests for the <see cref="SqlPatternAnalyzer"/> class to verify SQL pattern detection and analysis functionality.
+/// </summary>
 public class SqlPatternAnalyzerTests
 {
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.HasSelectStar(string)"/> method correctly identifies queries containing SELECT *.
+    /// </summary>
     [Fact]
     public void HasSelectStar_QueryContainsStar_ReturnsTrue()
     {
@@ -22,6 +28,9 @@ public class SqlPatternAnalyzerTests
         SqlPatternAnalyzer.HasSelectStar(query).Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.HasSelectStar(string)"/> method returns false when the query uses explicit column names instead of SELECT *.
+    /// </summary>
     [Fact]
     public void HasSelectStar_QueryWithNamedColumns_ReturnsFalse()
     {
@@ -32,6 +41,9 @@ public class SqlPatternAnalyzerTests
         SqlPatternAnalyzer.HasSelectStar(query).Should().BeFalse();
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.HasLeadingWildcardLike(string)"/> method detects LIKE patterns with leading wildcards (e.g., '%son') that prevent index usage.
+    /// </summary>
     [Fact]
     public void HasLeadingWildcardLike_PatternStartsWithPercent_ReturnsTrue()
     {
@@ -42,6 +54,9 @@ public class SqlPatternAnalyzerTests
         SqlPatternAnalyzer.HasLeadingWildcardLike(query).Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.DetectNPlusOnePattern(List{string})"/> method returns false when only a single query is provided, as N+1 pattern detection requires at least two queries.
+    /// </summary>
     [Fact]
     public void DetectNPlusOnePattern_SingleQueryInList_ReturnsFalse()
     {
@@ -52,6 +67,9 @@ public class SqlPatternAnalyzerTests
         SqlPatternAnalyzer.DetectNPlusOnePattern(queries).Should().BeFalse();
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.DetectNPlusOnePattern(List{string})"/> method detects N+1 query patterns when the same table is accessed more than five times.
+    /// </summary>
     [Fact]
     public void DetectNPlusOnePattern_SameTableAccessedMoreThanFiveTimes_ReturnsTrue()
     {
@@ -62,6 +80,9 @@ public class SqlPatternAnalyzerTests
         SqlPatternAnalyzer.DetectNPlusOnePattern(queries).Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.CalculateReadabilityScore(string)"/> method returns a perfect score of 100 for well-written queries with specific columns, explicit WHERE clauses, no implicit joins, and no wildcards.
+    /// </summary>
     [Fact]
     public void CalculateReadabilityScore_WellWrittenQuery_ReturnsFullScore()
     {
@@ -75,6 +96,9 @@ public class SqlPatternAnalyzerTests
         score.Should().Be(100.0);
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.CalculateReadabilityScore(string)"/> method deducts 30 points from the readability score when a query uses SELECT * with an implicit JOIN.
+    /// </summary>
     [Fact]
     public void CalculateReadabilityScore_SelectStarWithImplicitJoin_DeductsThirtyPoints()
     {
@@ -88,6 +112,9 @@ public class SqlPatternAnalyzerTests
         score.Should().Be(70.0);
     }
 
+    /// <summary>
+    /// Tests that the <see cref="SqlPatternAnalyzer.GenerateOptimizationRecommendations(string)"/> method includes advice to replace SELECT * with specific column names when analyzing a query that uses SELECT *.
+    /// </summary>
     [Fact]
     public void GenerateOptimizationRecommendations_SelectStarQuery_IncludesColumnReplacementAdvice()
     {
