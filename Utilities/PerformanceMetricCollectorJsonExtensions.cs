b0691cf80ebe353f1bed1b39ecaecacef02698f0
Vladyslav Zaiets <rutova2@gmail.com>
2020-06-29 12:43:36 +0300

    PerformanceMetricCollectorJson (de)serialization helpers
    also touched a couple of call sites.

---
+++ Utilities/PerformanceMetricCollectorJsonExtensions.cs
@@ -0,0 +1,83 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SqlQueryAnalyzer.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="PerformanceMetricCollector"/>.
+/// </summary>
+public static class PerformanceMetricCollectorJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="PerformanceMetricCollector"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The performance metric collector to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the performance metric collector.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this PerformanceMetricCollector value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="PerformanceMetricCollector"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A deserialized <see cref="PerformanceMetricCollector"/> instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static PerformanceMetricCollector? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<PerformanceMetricCollector>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="PerformanceMetricCollector"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful; otherwise, null.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out PerformanceMetricCollector? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        value = null;
+
+        try
+        {
+            value = JsonSerializer.Deserialize<PerformanceMetricCollector>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
