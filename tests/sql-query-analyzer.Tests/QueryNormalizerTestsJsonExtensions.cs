b32cb9492a97384eecaac1223545e5f384164dc5
Vladyslav Zaiets <rutova2@gmail.com>
2020-07-16 19:04:59 +0300

    Add QueryNormalizerTestsJson json helpers

--- tests/sql-query-analyzer.Tests/QueryNormalizerTestsJsonExtensions.cs
+++ tests/sql-query-analyzer.Tests/QueryNormalizerTestsJsonExtensions.cs
@@ -10,6 +10,9 @@ using System.Text.Json.Serialization;
 
 namespace SqlQueryAnalyzer.Tests;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="QueryNormalizerTests"/>.
+/// </summary>
 public static class QueryNormalizerTestsJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
@@ -19,37 +22,48 @@ public static class QueryNormalizerTestsJsonExtensions
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
     };
 
-    public static string ToJson(this QueryNormalizerTests value, bool indented = false)
-    {
-        if (value == null)
-        {
-            return "{}";
-        }
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
+    /// <summary>
+    /// Serializes a <see cref="QueryNormalizerTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize. Can be null.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the object.</returns>
+    public static string ToJson(this QueryNormalizerTests? value, bool indented = false)
+        => value is null
+            ? "{}"
+            : JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonOptions)
             {
                 WriteIndented = true
-            }
-            : _jsonOptions;
+            } : _jsonOptions);
 
-        return JsonSerializer.Serialize(value, options);
-    }
-
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="QueryNormalizerTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize. Must not be null or empty.</param>
+    /// <returns>The deserialized object, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static QueryNormalizerTests? FromJson(string json)
     {
-        try
-        {
-            return JsonSerializer.Deserialize<QueryNormalizerTests>(json, _jsonOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
+        ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+
+        return JsonSerializer.Deserialize<QueryNormalizerTests>(json, _jsonOptions);
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="QueryNormalizerTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize. Must not be null or empty.</param>
+    /// <param name="value">Receives the deserialized object if successful; otherwise, null.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static bool TryFromJson(string json, out QueryNormalizerTests? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json, nameof(json));
+
         try
         {
             value = JsonSerializer.Deserialize<QueryNormalizerTests>(json, _jsonOptions);
