bf850658884feb129b7a7ce69d6928b52c7f3260
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-04 09:37:10 +0300

    Json serialization for QueryAnalysisPipelineBenchmarksJson.

---
+++ benchmarks/sql-query-analyzer.Benchmarks/QueryAnalysisPipelineBenchmarksJsonExtensions.cs
@@ -0,0 +1,68 @@
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace SqlQueryAnalyzer.Benchmarks;
+
+/// <summary>
+/// Provides JSON serialization extensions for <see cref="QueryAnalysisPipelineBenchmarks"/>.
+/// </summary>
+public static class QueryAnalysisPipelineBenchmarksJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        WriteIndented = false,
+    };
+
+    /// <summary>
+    /// Converts a <see cref="QueryAnalysisPipelineBenchmarks"/> instance to its JSON representation.
+    /// </summary>
+    /// <param name="value">The benchmarks instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representing the benchmarks.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this QueryAnalysisPipelineBenchmarks value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, _jsonOptions with { WriteIndented = indented });
+    }
+
+    /// <summary>
+    /// Parses a JSON string into a <see cref="QueryAnalysisPipelineBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized benchmarks instance, or <see langword="null"/> if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
+    public static QueryAnalysisPipelineBenchmarks? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return JsonSerializer.Deserialize<QueryAnalysisPipelineBenchmarks>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to parse a JSON string into a <see cref="QueryAnalysisPipelineBenchmarks"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns><see langword="true"/> if parsing succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out QueryAnalysisPipelineBenchmarks? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<QueryAnalysisPipelineBenchmarks>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
