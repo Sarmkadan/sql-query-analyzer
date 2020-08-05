b9927277ed62a81a6f995505f1f3efea550ffbf2
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-05 08:20:19 +0300

    add QueryPlanAnalyzerTestsJson json helpers

---
+++ tests/sql-query-analyzer.Tests/QueryPlanAnalyzerTestsJsonExtensions.cs
@@ -0,0 +1,88 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace SqlQueryAnalyzer.Tests;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for <see cref="QueryPlanAnalyzerTests"/>.
+/// </summary>
+public static class QueryPlanAnalyzerTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+    };
+
+    /// <summary>
+    /// Serializes the test class to a JSON string.
+    /// </summary>
+    /// <param name="value">The test class to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
+    /// <returns>JSON representation of the test class</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+    public static string ToJson(this QueryPlanAnalyzerTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true,
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="QueryPlanAnalyzerTests"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>The deserialized test class, or null if the JSON is null or empty</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized</exception>
+    public static QueryPlanAnalyzerTests? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<QueryPlanAnalyzerTests>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="QueryPlanAnalyzerTests"/>.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">Receives the deserialized test class if successful</param>
+    /// <returns>True if deserialization succeeded; otherwise, false</returns>
+    public static bool TryFromJson(string json, out QueryPlanAnalyzerTests? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<QueryPlanAnalyzerTests>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
