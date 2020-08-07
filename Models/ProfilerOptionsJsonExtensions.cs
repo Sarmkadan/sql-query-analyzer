f07727e13fb31884034ad2c2d46337044045b48d
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-07 16:59:38 +0300

    feat(models): json serialization for ProfilerOptionsJson

---
+++ Models/ProfilerOptionsJsonExtensions.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SqlQueryAnalyzer.Models;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="ProfilerOptions"/>.
+/// </summary>
+public static class ProfilerOptionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    };
+
+    private static JsonSerializerOptions GetJsonOptions(bool indented)
+    {
+        var options = _jsonOptions;
+        if (indented)
+        {
+            options = new JsonSerializerOptions(_jsonOptions)
+            {
+                PropertyNamingPolicy = _jsonOptions.PropertyNamingPolicy,
+                WriteIndented = true,
+                DefaultIgnoreCondition = _jsonOptions.DefaultIgnoreCondition,
+            };
+        }
+        return options;
+    }
+
+    /// <summary>
+    /// Serializes the <see cref="ProfilerOptions"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The profiler options to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the profiler options.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this ProfilerOptions value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, GetJsonOptions(indented));
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a <see cref="ProfilerOptions"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized profiler options, or <see langword="null"/> if the JSON represents a null value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ProfilerOptions? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return JsonSerializer.Deserialize<ProfilerOptions>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a <see cref="ProfilerOptions"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized profiler options, or <see langword="null"/> if the JSON represents a null value.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out ProfilerOptions? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<ProfilerOptions>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = default;
+            return false;
+        }
+    }
+}
