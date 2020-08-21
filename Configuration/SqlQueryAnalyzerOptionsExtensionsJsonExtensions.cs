38082df3fecde82cbea0f932ac08f01b3d91b761
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-21 20:10:22 +0300

    Serialize SqlQueryAnalyzerOptionsExtensionsJson properly

---
+++ Configuration/SqlQueryAnalyzerOptionsExtensionsJsonExtensions.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Text.Json;
+
+namespace SqlQueryAnalyzer.Configuration;
+
+/// <summary>
+/// Provides JSON serialization helpers for <see cref="SqlQueryAnalyzerOptionsExtensions"/> extension methods.
+/// </summary>
+public static class SqlQueryAnalyzerOptionsExtensionsJsonExtensions
+{
+    /// <summary>
+    /// Configured JSON serializer options with camelCase naming policy.
+    /// </summary>
+    private static readonly JsonSerializerOptions JsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="SqlQueryAnalyzerOptions"/> instance to JSON.
+    /// </summary>
+    /// <param name="value">The options to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>JSON string representation of the options.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this SqlQueryAnalyzerOptions value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(JsonOptions) { WriteIndented = true }
+            : JsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="SqlQueryAnalyzerOptions"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized options, or null if input is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="JsonException">Thrown when JSON is invalid.</exception>
+    public static SqlQueryAnalyzerOptions? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        return JsonSerializer.Deserialize<SqlQueryAnalyzerOptions>(json, JsonOptions);
+    }
+
+    /// <summary>
+    /// Tries to deserialize a JSON string to a <see cref="SqlQueryAnalyzerOptions"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized options, or null on failure.</param>
+    /// <returns>True if deserialization succeeded, false otherwise.</returns>
+    public static bool TryFromJson(string json, out SqlQueryAnalyzerOptions? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<SqlQueryAnalyzerOptions>(json, JsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
