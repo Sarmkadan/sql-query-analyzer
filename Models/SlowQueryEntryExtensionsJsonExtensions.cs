070a4a556633252e01195989c7264c811739327b
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-21 17:52:21 +0300

    feat(models): SlowQueryEntryExtensionsJson (de)serialization helpers

---
+++ Models/SlowQueryEntryExtensionsJsonExtensions.cs
@@ -0,0 +1,89 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// ====================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SqlQueryAnalyzer.Models;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="SlowQueryEntryExtensions"/>
+/// to enable JSON serialization of <see cref="SlowQueryEntry"/> instances.
+/// </summary>
+public static class SlowQueryEntryExtensionsJsonExtensions
+{
+    /// <summary>
+    /// Shared JSON serialization options with camelCase property naming.
+    /// </summary>
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="SlowQueryEntry"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The slow query entry to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the slow query entry.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this SlowQueryEntry value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="SlowQueryEntry"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized slow query entry, or <see langword="null"/> if the JSON is empty or invalid.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+    public static SlowQueryEntry? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            return JsonSerializer.Deserialize<SlowQueryEntry>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="SlowQueryEntry"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized slow query entry if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out SlowQueryEntry? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<SlowQueryEntry>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
