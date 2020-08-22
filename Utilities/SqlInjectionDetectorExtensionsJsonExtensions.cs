ad65cffa043664c3b1331bfde3ebfc62abe82795
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-22 10:52:37 +0300

    tidy up SqlInjectionDetectorExtensionsJson json handling

---
+++ Utilities/SqlInjectionDetectorExtensionsJsonExtensions.cs
@@ -0,0 +1,76 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System;
+using System.Text.Json;
+
+namespace SqlQueryAnalyzer.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for SqlInjectionDetectorExtensions.
+/// </summary>
+public static class SqlInjectionDetectorExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a type marker representing SqlInjectionDetectorExtensions to a JSON string.
+    /// </summary>
+    /// <param name="value">This parameter is ignored; only the type context is used.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the SqlInjectionDetectorExtensions type marker.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this object value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(new { Type = nameof(SqlInjectionDetectorExtensions) }, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into a type marker representing SqlInjectionDetectorExtensions.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A type marker object, or <see langword="null"/> if the JSON is empty.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static object? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<object>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into a type marker representing SqlInjectionDetectorExtensions.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized type marker if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    public static bool TryFromJson(string json, out object? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<object>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
