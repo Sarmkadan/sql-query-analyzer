59b3ef5d743e4ec99296e681881ed74dc7e56e75
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-14 07:23:08 +0000

    feat(validation): add ValidationRuleEngine extension helpers

---
+++ Validation/ValidationRuleEngineExtensions.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace SqlQueryAnalyzer.Validation;
+
+/// <summary>
+/// Provides extension methods for <see cref="ValidationRuleEngine"/>.
+/// </summary>
+public static class ValidationRuleEngineExtensions
+{
+    /// <summary>
+    /// Validates a query and checks if it is valid.
+    /// </summary>
+    /// <param name="engine">The validation rule engine.</param>
+    /// <param name="query">The SQL query to validate.</param>
+    /// <returns>True if the query is valid; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when engine is null.</exception>
+    public static bool IsValidQuery(this ValidationRuleEngine engine, string query)
+    {
+        ArgumentNullException.ThrowIfNull(engine);
+        return engine.ValidateQuery(query).IsValid;
+    }
+
+    /// <summary>
+    /// Validates a query and returns the count of errors found.
+    /// </summary>
+    /// <param name="engine">The validation rule engine.</param>
+    /// <param name="query">The SQL query to validate.</param>
+    /// <returns>The number of errors.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when engine is null.</exception>
+    public static int GetErrorCount(this ValidationRuleEngine engine, string query)
+    {
+        ArgumentNullException.ThrowIfNull(engine);
+        return engine.ValidateQuery(query).Errors.Count;
+    }
+
+    /// <summary>
+    /// Validates a query and returns the count of warnings found.
+    /// </summary>
+    /// <param name="engine">The validation rule engine.</param>
+    /// <param name="query">The SQL query to validate.</param>
+    /// <returns>The number of warnings.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when engine is null.</exception>
+    public static int GetWarningCount(this ValidationRuleEngine engine, string query)
+    {
+        ArgumentNullException.ThrowIfNull(engine);
+        return engine.ValidateQuery(query).Warnings.Count;
+    }
+}
