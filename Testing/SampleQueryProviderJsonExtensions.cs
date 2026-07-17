08dcea8621a96e3aea8a164861b29a57f95fb950
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    small refactor - SampleQueryProviderJsonExtensions

--- Testing/SampleQueryProviderJsonExtensions.cs
+++ Testing/SampleQueryProviderJsonExtensions.cs
@@ -8,81 +8,77 @@ namespace SqlQueryAnalyzer.Testing;
 /// </summary>
 public static class SampleQueryProviderJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
-        WriteIndented = false
-    };
+	private sealed class SampleQueryData
+	{
+		public required Dictionary<string, string> AllSamples { get; init; }
+		public required string RandomSample { get; init; }
+		public required Dictionary<string, List<string>> SamplesByIssueType { get; init; }
+	}
 
-    /// <summary>
-    /// Serializes the sample queries to a JSON string.
-    /// </summary>
-    /// <param name="value">The <see cref="SampleQueryProvider"/> type to serialize data from.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the sample queries.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
-    public static string ToJson(bool indented = false)
-    {
-        var samples = SampleQueryProvider.GetAllSamples();
-        var randomSample = SampleQueryProvider.GetRandomSample();
-        var byIssueType = SampleQueryProvider.GetSamplesByIssueType();
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+		WriteIndented = false
+	};
 
-        var data = new
-        {
-            AllSamples = samples,
-            RandomSample = randomSample,
-            SamplesByIssueType = byIssueType
-        };
+	/// <summary>
+	/// Serializes the sample queries to a JSON string.
+	/// </summary>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the sample queries.</returns>
+	public static string ToJson(bool indented = false)
+	{
+		var data = new SampleQueryData
+		{
+			AllSamples = SampleQueryProvider.GetAllSamples(),
+			RandomSample = SampleQueryProvider.GetRandomSample(),
+			SamplesByIssueType = SampleQueryProvider.GetSamplesByIssueType()
+		};
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions;
 
-        return JsonSerializer.Serialize(data, options);
-    }
+		return JsonSerializer.Serialize(data, options);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string to sample query data.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized sample query data, or <see langword="null"/> if the JSON is empty.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static object? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+	/// <summary>
+	/// Deserializes a JSON string to sample query data.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized sample query data, or <see langword="null"/> if the JSON is empty.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static object? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+		return string.IsNullOrWhiteSpace(json)
+			? null
+			: JsonSerializer.Deserialize<object>(json, _jsonOptions);
+	}
 
-        return JsonSerializer.Deserialize<object>(json, _jsonOptions);
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to sample query data.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized sample query data if successful, otherwise <see langword="null"/>.</param>
+	/// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
+	public static bool TryFromJson(string json, out object? value)
+	{
+		ArgumentException.ThrowIfNullOrEmpty(json);
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to sample query data.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized sample query data if successful, otherwise <see langword="null"/>.</param>
-    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
-    public static bool TryFromJson(string json, out object? value)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
-
-        try
-        {
-            value = JsonSerializer.Deserialize<object>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+		try
+		{
+			value = JsonSerializer.Deserialize<object>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
