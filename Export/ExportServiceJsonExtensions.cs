db085919be279fbdee5bf1584acb1ef906d1fe5f
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor: dedupe some logic in ExportServiceJsonExtensions.

--- Export/ExportServiceJsonExtensions.cs
+++ Export/ExportServiceJsonExtensions.cs
@@ -34,6 +34,7 @@ public static class ExportServiceJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized export service instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/> or empty.</exception>
     /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
     public static ExportService? FromJson(string json)
     {
@@ -48,6 +49,7 @@ public static class ExportServiceJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized export service instance if successful.</param>
     /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static bool TryFromJson(string json, out ExportService? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
